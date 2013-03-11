c93cc31c286e4932924d29e380fab7df3bb2edf5
Pascal Opitz <contact@pascalopitz.com>
2013-03-11 21:08:11 +1030

    BEANSTALK_PORT env, slight change to test helper

--- tests/helper.js
+++ tests/helper.js
@@ -1,7 +1,7 @@
 var bs = require('../lib/beanstalk_client');
 var net = require('net');
 
-var port = process.env.PORT || 11333;
+var port = process.env.BEANSTALK_PORT || 11333;
 
 var mock = process.env.BEANSTALKD !== '1';
 var mock_server;
@@ -11,30 +11,31 @@ var connection;
 module.exports = {
 	bind : function (fn, closeOnEnd) {
 
-		if(mock) {
-			mock_server = net.createServer(function(conn) {
-				connection = conn;
-
-				connection.on('data', function (data) {
-					fn.call(mock_server, connection, data);
-				});
-
-				if(closeOnEnd === true) {
-					closeOnEnd = function () {
-						mock_server.close();
-					}
-				}
-
-				if(closeOnEnd) {
-					connection.on('end', function () {
-						closeOnEnd.call(mock_server); 
-					});
-				}
-			});
-
-			mock_server.listen(port);
+		if(!mock) {
+			return false;
 		}
 
+		mock_server = net.createServer(function(conn) {
+			connection = conn;
+
+			connection.on('data', function (data) {
+				fn.call(mock_server, connection, data);
+			});
+
+			if(closeOnEnd === true) {
+				closeOnEnd = function () {
+					mock_server.close();
+				}
+			}
+
+			if(closeOnEnd) {
+				connection.on('end', function () {
+					closeOnEnd.call(mock_server); 
+				});
+			}
+		});
+
+		mock_server.listen(port);
 	},
 	getClient : function () {
 		return bs.Client('127.0.0.1:' + port);
